6f93a448ca147632d26ff496c954d847622921e0
Alfred Reynold <reynoldalfred@gmail.com>
2015-06-22 08:16:39 +0530

    well started

---
+++ js/reno.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+	"use strict";
+	function scrollTo() {
+		$('.item').click(function(){  
+        //Toggle Class
+        $(".active").removeClass("active");      
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 10
+        }, 400);
+        return false;
+      });
+      $('.scrollTop a').scrollTop();
+	}
+	scrollTo();
+};
+
